0c8aefbfadea9b1e441cb4add8fcd96b553a5af0
rharpw <roddy.weir@gmail.com>
2024-05-21 23:23:52 +0100

    refactoring monsters into dictionaries

---
+++ Game/main.py
@@ -0,0 +1,5 @@
+import monsters
+from monsters import Monster
+import random
+
+x = random.randint(1, 9)
--- Game/monsters.py
+++ Game/monsters.py
@@ -18,7 +18,7 @@ class Monster:
         """Creature attack message."""
         attack_message = f"{self.species} is attacking!"
 
-    def creatue_defend(self):
+    def creature_defend(self):
         """Creature defend message."""
         defend_message = f"{self.species} is defending!"
 
@@ -28,12 +28,78 @@ class Monster:
         return long_desc
 
 
-monster_01 = Monster("Zombie", 5, 2, 3, 1, 0)
-monster_02 = Monster("Mummy", 4, 3, 4, 2, 0)
-monster_03 = Monster("Gargoyle", 6, 4, 5, 3, 0)
-monster_04 = Monster("Orc", 8, 3, 2, 1, 2)
-monster_05 = Monster("Skeleton", 6, 2, 2, 1, 0)
-monster_06 = Monster("Goblin", 10, 2, 1, 1, 1)
-monster_07 = Monster("Chaos Warrior", 7, 4, 4, 3, 3)
-monster_08 = Monster("Fimir", 6, 3, 3, 2, 3)
-monster_09 = Monster("Chaos Warlock", 8, 3, 3, 1, 4)
+creatures = {
+    "monster01": {
+        "species": "Zombie",
+        "movement": 5,
+        "attack": 2,
+        "defend": 3,
+        "hp": 1,
+        "mp": 1,
+    },
+    "monster02": {
+        "species": "Gargoyle",
+        "movement": 6,
+        "attack": 4,
+        "defend": 5,
+        "hp": 3,
+        "mp": 0,
+    },
+    "monster03": {
+        "species": "Gargoyle",
+        "movement": 6,
+        "attack": 4,
+        "defend": 5,
+        "hp": 3,
+        "mp": 0,
+    },
+    "monster04": {
+        "species": "Orc",
+        "movement": 8,
+        "attack": 3,
+        "defend": 2,
+        "hp": 1,
+        "mp": 2,
+    },
+    "monster05": {
+        "species": "Skeleton",
+        "movement": 6,
+        "attack": 2,
+        "defend": 2,
+        "hp": 1,
+        "mp": 0,
+    },
+    "monster06": {
+        "species": "Goblin",
+        "movement": 10,
+        "attack": 2,
+        "defend": 1,
+        "hp": 1,
+        "mp": 1,
+    },
+    "monster07": {
+        "species": "Chaos Warrior",
+        "movement": 7,
+        "attack": 4,
+        "defend": 4,
+        "hp": 3,
+        "mp": 3,
+    },
+    "monster08": {
+        "species": "Fimir",
+        "movement": 6,
+        "attack": 3,
+        "defend": 3,
+        "hp": 2,
+        "mp": 3,
+    },
+    "monster09": {
+        "species": "Chaos Warlock",
+        "movement": 8,
+        "attack": 3,
+        "defend": 3,
+        "hp": 1,
+    },
+}
+
+print(creatures["monster06"])
